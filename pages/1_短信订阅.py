17431e302b94937c15587f294cf16c8e83e7e65d
claude89757 <claudexie@outlook.com>
2024-08-16 02:31:27 +0800

    test

--- pages/1_短信订阅.py
+++ pages/1_短信订阅.py
@@ -215,6 +215,7 @@ with tab2:
                         # 删除按钮
                         st.write(f"订阅ID: {row['订阅ID']}")
                         if st.button(f"删除订阅 {index + 1}", key=f"delete_button_{index}", type="primary"):
+                            st.warning("????????????")
                             st.query_params.del_subscription_id = row['订阅ID']
                             st.session_state.del_subscription_id = row['订阅ID']
                             time.sleep(3)
