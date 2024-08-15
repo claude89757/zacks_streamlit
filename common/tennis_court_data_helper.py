8ad4435c9e320feffd165fa8878cf6a3b1d93292
claude89757 <claudexie@outlook.com>
2024-08-16 00:03:50 +0800

    fix

--- common/tennis_court_data_helper.py
+++ common/tennis_court_data_helper.py
@@ -119,7 +119,7 @@ def set_realtime_tennis_court_sheet():
                         start_time, end_time = slot
                         if start_time in table_data and date in table_data[start_time]:
                             if court_name not in table_data[start_time][date]:
-                                table_data[start_time][date] += f"|{court_name}:{court_index}"
+                                table_data[start_time][date] += f"|{court_name}  {court_index}"
                             else:
                                 table_data[start_time][date] += f",{court_index}"
                             table_data[start_time][date] = table_data[start_time][date].strip("|")
